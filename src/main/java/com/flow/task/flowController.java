b4d7b8affd0fda115be4fc498690a29832543ae0
gyunny <95176511+taegyun1995@users.noreply.github.com>
2023-09-22 17:06:35 +0900

    feat: create a view for fixed extension assignments (#2)

---
+++ src/main/java/com/flow/task/flowController.java
@@ -0,0 +1,14 @@
+package com.flow.task;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class flowController {
+
+    @GetMapping("/flow")
+    public String signup() {
+        return "/index";
+    }
+
+}
