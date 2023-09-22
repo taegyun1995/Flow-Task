942064dd64b6190998b8d6ce7591819c76293d4f
gyunny <95176511+taegyun1995@users.noreply.github.com>
2023-09-23 00:45:37 +0900

    feat: list of custom extensions view raw (#6)

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,6 @@
 const fixedExtensions = ["bat", "cmd", "com", "cpl", "exe", "src", "js"];
 const fixedExtensionsDiv = document.getElementById("fixed-extensions-items");
+const customExtensions = [];
 
 function displayFixedExtensions() {
   fixedExtensionsDiv.innerHTML = "";
@@ -17,7 +18,44 @@ function displayFixedExtensions() {
 
 const deleteSVG = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>`;
 
+function displayCustomExtensions() {
+  const selectedExtensionsSpan = document.getElementById(
+    "added-items-extensions"
+  );
+  selectedExtensionsSpan.innerHTML = "";
+
+  customExtensions.forEach((ext) => {
+    const newDiv = document.createElement("span");
+    newDiv.className = "extension-item";
+
+    const extName = document.createElement("span");
+    extName.textContent = ext;
+    newDiv.appendChild(extName);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "added-button";
+    deleteBtn.innerHTML = deleteSVG;
+    deleteBtn.onclick = function () {
+      const index = customExtensions.indexOf(ext);
+      if (index > -1) {
+        customExtensions.splice(index, 1);
+      }
+      displayCustomExtensions();
+    };
+    newDiv.appendChild(deleteBtn);
+
+    selectedExtensionsSpan.appendChild(newDiv);
+  });
+  updateSelectedCount();
+}
+
 function addCustomExtension() {
+  if (customExtensions.length >= MAX_SELECTED) {
+    alert("최대 200개의 확장자만 추가할 수 있습니다.");
+    document.getElementById("custom-extension-input").value = "";
+    return;
+  }
+
   const inputValue = document
     .getElementById("custom-extension-input")
     .value.trim();
@@ -28,6 +66,20 @@ function addCustomExtension() {
     document.getElementById("custom-extension-input").value = "";
     return;
   }
+
+  if (inputValue && !customExtensions.includes(inputValue)) {
+    customExtensions.push(inputValue);
+    displayCustomExtensions();
+    document.getElementById("custom-extension-input").value = "";
+  }
+}
+
+const MAX_SELECTED = 200;
+const selectedCountSpan = document.getElementById("added-items-count");
+
+function updateSelectedCount() {
+  selectedCountSpan.textContent = `${customExtensions.length}/${MAX_SELECTED}`;
 }
 
 displayFixedExtensions();
+displayCustomExtensions();
