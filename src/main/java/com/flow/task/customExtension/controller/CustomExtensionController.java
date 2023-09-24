9d361ff5e81b7ec75417edf8a092a5db4d5011a8
taegyun1995 <needle12s@gmail.com>
2023-09-24 13:48:26 +0900

    feat: create an api to delete custom extensions

--- src/main/java/com/flow/task/customExtension/controller/CustomExtensionController.java
+++ src/main/java/com/flow/task/customExtension/controller/CustomExtensionController.java
@@ -2,11 +2,10 @@ package com.flow.task.customExtension.controller;
 
 import com.flow.task.customExtension.request.CreateCustomExtensionRequest;
 import com.flow.task.customExtension.response.CreateCustomExtensionResponse;
+import com.flow.task.customExtension.response.DeleteCustomExtensionResponse;
 import com.flow.task.customExtension.service.CustomExtensionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomExtensionController {
@@ -20,6 +19,12 @@ public class CustomExtensionController {
     @PostMapping("/custom/extension")
     public ResponseEntity<CreateCustomExtensionResponse> addCustomExtension(@RequestBody CreateCustomExtensionRequest request) {
         CreateCustomExtensionResponse response = customExtensionService.addCustomExtension(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/custom/extension/{id}/delete")
+    public ResponseEntity<DeleteCustomExtensionResponse> deleteCustomExtension(@PathVariable Long id) {
+        DeleteCustomExtensionResponse response = customExtensionService.deleteCustomExtension(id);
         return ResponseEntity.ok(response);
     }
 
