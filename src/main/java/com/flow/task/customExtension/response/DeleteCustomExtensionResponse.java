6aa48f7ed2d9be07defcace9238a3617f4810b15
taegyun1995 <needle12s@gmail.com>
2023-09-24 14:31:58 +0900

    feat: add default constructors to custom extension-related dtos

--- src/main/java/com/flow/task/customExtension/response/DeleteCustomExtensionResponse.java
+++ src/main/java/com/flow/task/customExtension/response/DeleteCustomExtensionResponse.java
@@ -1,8 +1,11 @@
 package com.flow.task.customExtension.response;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeleteCustomExtensionResponse {
 
     private String CustomExtensionName;
