5a4c9eba882a638657645d78b074886878e17194
taegyun1995 <needle12s@gmail.com>
2023-09-25 16:24:35 +0900

    deploy: changing from a jar to j war deployment

--- src/main/java/com/flow/task/FlowApplication.java
+++ src/main/java/com/flow/task/FlowApplication.java
@@ -1,13 +1,16 @@
 package com.flow.task;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class FlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowApplication.class, args);
+    }
 
 }
