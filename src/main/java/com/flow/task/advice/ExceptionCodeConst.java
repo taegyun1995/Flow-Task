100d9fe4683ccf01f34e10c7f2c20018c9b97e0a
taegyun1995 <needle12s@gmail.com>
2023-09-25 21:49:37 +0900

    fix: fixed extensions should not be represented in custom extensions

--- src/main/java/com/flow/task/advice/ExceptionCodeConst.java
+++ src/main/java/com/flow/task/advice/ExceptionCodeConst.java
@@ -8,6 +8,7 @@ public enum ExceptionCodeConst {
     NOT_FOUND_FIXED_EXTENSION(404, "NOT_FOUND_FIXED_EXTENSION", "고정 확장자를 찾을 수 없습니다."),
     NOT_FOUND_CUSTOM_EXTENSION(404, "NOT_FOUND_CUSTOM_EXTENSION", "커스텀 확장자를 찾을 수 없습니다."),
 
+    ALREADY_FIXED_EXIST_EXTENSION(409, "ALREADY_FIXED_EXIST_EXTENSION", "이미 고정 확장자로 등록되었습니다."),
     ALREADY_EXIST_EXTENSION(409, "ALREADY_EXIST_EXTENSION", "이미 등록된 확장자입니다."),
 
     LENGTH_EXTENSION_NAME(416, "LENGTH_EXTENSION_NAME", "확장자의 길이를 20글자 이하로 작성해주세요."),
--- src/main/java/com/flow/task/fixedExtension/repository/FixedExtensionRepository.java
+++ src/main/java/com/flow/task/fixedExtension/repository/FixedExtensionRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 
 public interface FixedExtensionRepository extends JpaRepository<FixedExtensions, Long> {
 
+    Boolean existsByExtensionName(String extensionName);
+
     Optional<FixedExtensions> findByIdAndStatus(Long id, ExtensionStatus status);
 
 }
